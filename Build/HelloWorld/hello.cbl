5a3aabb05a9a86b2abf0a32af795a73270a36aab
Shalini174 <39622811+Shalini174@users.noreply.github.com>
2020-02-11 02:22:04 -0500

    Update hello.cbl

--- Build/HelloWorld/hello.cbl
+++ Build/HelloWorld/hello.cbl
@@ -8,6 +8,7 @@
 
        01  RCV-PARMS.
            02  ACCOUNT-BALANCE  PIC 9(9) VALUE ZERO.
+           02  ACCOUNT-BAL-REM  PIC 9(9) VALUE ZERO.
            02  AMOUNT           PIC 9(9) VALUE ZERO.
            02  ACTION           PIC X(10) VALUE SPACES.
 
@@ -15,10 +16,10 @@
        0001-MAIN.
            DISPLAY 'INSIDE 0001-MAIN PARA'.
            IF ACTION = 'DEBIT'
-            COMPUTE ACCOUNT-BALANCE = ACCOUNT-BALANCE - AMOUNT
+            COMPUTE ACCOUNT-BAL-REM = ACCOUNT-BALANCE - AMOUNT
            ELSE
              IF ACTION = 'CREDIT'
-               COMPUTE ACCOUNT-BALANCE = ACCOUNT-BALANCE + AMOUNT
+               COMPUTE ACCOUNT-BAL-REM = ACCOUNT-BALANCE + AMOUNT
              END-IF
            END-IF
            GOBACK.
